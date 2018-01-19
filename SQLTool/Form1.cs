93129d6cfba81ebecef94a1ebae76b2959e06208
Michał Cedro <michalcedro88@gmail.com>
2018-01-19 14:39:25 +0100

    Test gita

--- SQLTool/Form1.cs
+++ SQLTool/Form1.cs
@@ -12,9 +12,33 @@ namespace SQLTool
 {
     public partial class Form1 : Form
     {
+
+        static int visibleAuth; //check radiobutton
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            visibleAuth = 1;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (visibleAuth==1)
+            {
+                label2.Visible = false;
+                label3.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                visibleAuth = 0;
+            }      
         }
     }
 }
