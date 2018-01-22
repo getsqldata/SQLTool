71449fc1cd988fc3f5a9400782e22049720c3552
Michał Cedro <michalcedro88@gmail.com>
2018-01-22 20:47:56 +0100

    Check try catch

--- SQLTool/Klasy/Logic.cs
+++ SQLTool/Klasy/Logic.cs
@@ -23,23 +23,35 @@ namespace SQLTool.Klasy
             //string connectionString = "Data Source=MCEDRO-DELL\\SQLSRV; Integrated Security=True;";
             string connectionString = "Data Source=" + Form1.nameServer + "; Integrated Security=True;";
 
-            using (SqlConnection con = new SqlConnection(connectionString))
+            try
             {
-                con.Open();
-                using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con))
-                {
-                    using (SqlDataReader db = cmd.ExecuteReader())
-                    {
-                        while (db.Read())
-                        {
-                            dbases.Add(db[0].ToString());
-                            //MessageBox.Show(db[0].ToString());
-                        }
-                        db.Close();
-                    }
-                }
 
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con))
+                    {
+                        using (SqlDataReader db = cmd.ExecuteReader())
+                        {
+                            while (db.Read())
+                            {
+                                dbases.Add(db[0].ToString());
+                                //MessageBox.Show(db[0].ToString());
+                            }
+                            db.Close();
+                        }
+                    }
+
+                }
+            } catch
+
+            {
+                MessageBox.Show("UPS - Check server name");
             }
+
+
+
+
             return dbases;
 
         }        
